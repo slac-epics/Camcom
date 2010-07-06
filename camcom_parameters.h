62c32bc6fece64d91a70c66739d7cdf7f93c8d27
ronc <>
2010-07-06 23:56:27 +0000

    First version of CAMCOM to CVS

---
+++ camcom_parameters.h
@@ -0,0 +1,67 @@
+/*
+
+Abs: CAMCOM single packet paramaters
+
+Name: camcom_parameters.h
+
+Auth: 26-Jan-2010  R. Chestnut
+
+*/
+
+#ifndef camcom_parameters_h
+
+#define exit_index 0 
+#define hex_index 1
+#define piop_checksum_index 2
+#define re_pack_index 3
+#define longword_index 4
+#define repeat_index 5
+#define p32_index 6
+#define p24_index 7
+#define p16_index 8
+#define p8_index 9
+#define ilq_index 10
+#define quiet_index 11
+#define verify_index 12
+#define sa_index 13
+#define sc_index 14
+#define sn_index 15
+#define qm1_index 16
+#define qm2_index 17
+#define qm3_index 18
+#define xm1_index 19
+#define xm2_index 20
+#define xm3_index 21
+#define f_index 22
+#define nofill_index 23
+#define nobcnt_index 24
+#define nodata_index 25
+#define noprompt_index 26
+#define nodebug_index 27
+#define noinput_index 28
+#define nooutput_index 29
+#define N_noval_index 30
+
+#define branch_index 0
+#define module_index 1
+#define crate_index 2
+#define fn_index 3
+#define address_index 4
+#define ctlw_index 5
+#define fill_index 6
+#define sleep_index 7
+#define bcnt_index 8
+#define command_index 9
+#define data_index 10
+#define prompt_index 11
+#define debug_index 12
+#define input_index 13
+#define output_index 14
+#define N_val_index 15
+
+#define MBCD_LEN 6
+#define EMASK_LEN 1
+#define CTLBLK_LEN 8
+
+#define camcom_parameters_h
+#endif
